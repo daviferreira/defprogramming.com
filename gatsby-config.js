3358a182077c823a16ab558f29c3e1e03e768443
Davi Ferreira <contato@daviferreira.com>
2019-10-05 15:14:03 +0200

    Add data

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,9 +26,17 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
       }
-    }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`
+      }
+    }
   ]
 };
